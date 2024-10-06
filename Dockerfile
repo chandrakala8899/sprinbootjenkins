a075d67073303bbe3b9a846910681bdd1c392aac
chandrakala <chandraklag688@gmail.com>
2024-10-06 10:37:56 +0530

    jenkinsdemo

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM openjdk:17-jdk-alpine
+COPY ./target/springbootjenkinsdemo-1.0-SNAPSHOT.jar springbootjenkinsdemo.jar
+
+ENTRYPOINT ["java","-jar","/springbootjenkinsdemo.jar"]
--- src/main/java/com/neoteric/springbootjenkinsdemo/App.java
+++ src/main/java/com/neoteric/springbootjenkinsdemo/App.java
@@ -1,13 +1,13 @@
 package com.neoteric.springbootjenkinsdemo;
 
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ){
+      System.out.println( "Hello World!" );
     }
 }
