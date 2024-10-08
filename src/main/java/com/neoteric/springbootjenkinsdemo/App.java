bde12cab356faf25cc2fc0132271c885263a256d
chandrakala <chandraklag688@gmail.com>
2024-10-08 12:06:04 +0530

    jenkinsdemo

--- src/main/java/com/neoteric/springbootjenkinsdemo/App.java
+++ src/main/java/com/neoteric/springbootjenkinsdemo/App.java
@@ -8,6 +8,6 @@ package com.neoteric.springbootjenkinsdemo;
 public class App 
 {
     public static void main( String[] args ){
-      System.out.println( "Hello World!" );
+      System.out.println( "Hello World from springboot" );
     }
 }
